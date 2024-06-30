eb206e7757468617a50b39bee6751d5ff0746e78
CodeByBlazej <codebyblazej@gmail.com>
2024-06-30 12:18:02 +0100

    finish project with much cleaner loops

--- app.rb
+++ app.rb
@@ -1,46 +1,27 @@
-require 'pry-byebug'
-
 def stock_picker (prices)
-  daily_stock_prices = []
+
+  days = []
 
   max_prices = prices.max(prices.length)
   
-  
-  prices.each do |stock_price|
-      max_prices.each do |max_price|
-        if stock_price < max_price
-          break if daily_stock_prices.length > 0
-          daily_stock_prices.push(stock_price)
-        end
+
+  prices.each_with_index do |stock_price, index|
+    if stock_price < max_prices[index]
+      days.push(index)
+      break if days.length == 1
+    end
+  end
+
+  if !days.empty?
+    prices.each_with_index do |stock_price, index|
+      if stock_price > max_prices[index]
+        days.push(index)
+        break if days.length == 2
       end
     end
+  end
 
-    if !daily_stock_prices.empty?
-      max_prices.each do |max_price|
-        prices.each do |stock_price|
-          if max_price < stock_price
-            break if daily_stock_prices.length > 1
-            daily_stock_prices.push(max_price)
-          end
-        end
-      end
-    end
-
-    days = daily_stock_prices.map {|price| prices.index(price)}
-    
-    p days
-
-  # prices.each do |stock_price|
-  #   max_prices.each do |max_price|
-  #     if stock_price < max_price
-  #       days.push(prices.index(stock_price))
-  #       break
-  #     end
-  #   end
-  # end
-
-  binding.pry
-
+  p days
 
 end
 
